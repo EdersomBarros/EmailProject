43348ea10a6716bb442c52812479852ce482f12c
Perkles <edersombarros@hotmail.com>
2022-07-17 22:13:50 -0400

    Rodando

--- src/br/com/emailproject/service/EmailService.java
+++ src/br/com/emailproject/service/EmailService.java
@@ -52,8 +52,8 @@ public class EmailService extends Thread {
 		Properties properties = new Properties();
 
 		properties.put("mail.smtp.starttls.enable", true);
-		properties.put("mail.smtp.host", System.getProperty("email.project.mail.smtp.host"));
-		properties.put("mail.smtp.port", System.getProperty("email.project.mail.smtp.port"));
+		properties.put("mail.smtp.host", System.getProperty("email-project.mail.smtp.host"));
+		properties.put("mail.smtp.port", System.getProperty("email-project.mail.smtp.port"));
 
 		Session session = Session.getInstance(properties);
 		session.setDebug(false);
@@ -61,7 +61,7 @@ public class EmailService extends Thread {
 		for (Email email : emails) {
 			try {
 				Message message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(System.getProperty("email.project.mail.from")));
+				message.setFrom(new InternetAddress(System.getProperty("email-project.mail.from")));
 
 				if (email.getDestinatario().contains("/")) {
 					List<InternetAddress> emailsLocal = new ArrayList<>();
